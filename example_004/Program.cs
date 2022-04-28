8c50de09a7be65871df946497d0066263879ab2c
gheorghe <tyson13.88@mail.ru>
2022-04-29 00:12:34 +0300

    rabota c#2

---
+++ example_004/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("vedite vashe imea: ");
+string usernume = Console.ReadLine();
+
+if(usernume.ToLower() == "Kristina")//aici este o gresala,dar nustiu unde.
+{
+    Console.WriteLine("moia jena samaia lucishaia");
+}
+else
+{
+    Console.Write("privet, ");
+    Console.WriteLine(usernume);
+}
