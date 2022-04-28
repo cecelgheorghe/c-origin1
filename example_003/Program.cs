74b504c7305893ce5e65b0b0a32a4a9f4f4eb10c
gheorghe <tyson13.88@mail.ru>
2022-04-28 15:13:27 +0300

    uyjrcwbejkcn

---
+++ example_003/Program.cs
@@ -0,0 +1,8 @@
+﻿//int numberA = 20;
+//int numberB = 10;
+//Console.WriteLine(numberA + numberB);
+//prima metoda de aduna si acaderea.
+//doua sleshuri este un comentariu.
+double numberA = 98;
+double numberB = 5;
+Console.WriteLine(numberA / numberB);
